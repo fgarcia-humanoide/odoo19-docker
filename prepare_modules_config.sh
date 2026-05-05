7463a6e768917ec49de3b5bf48802945e1d8d62c
Fernando García Hansen <fgarcia@humanoide.es>
2026-05-05 18:21:09 +0200

    Update prepare_modules_config.sh

--- prepare_modules_config.sh
+++ prepare_modules_config.sh
@@ -8,7 +8,7 @@ mkdir -p /data/compose/1/addons/otros
 
 echo "📦 Copiando módulos..."
 cp -r /root/odoo19-docker/login_user_detail /data/compose/1/addons/otros/ 2>/dev/null || true
-cp -r /root/odoo19-docker/supermodulo18 /data/compose/1/addons/otros/ 2>/dev/null || true
+cp -r /root/odoo19-docker/supermodulo19 /data/compose/1/addons/otros/ 2>/dev/null || true
 
 echo "📄 Copiando scripts y ficheros de configuración..."
 cp /root/odoo19-docker/modules_install_19.sh /data/compose/1/addons/ 2>/dev/null || true
